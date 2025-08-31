9fc253bb53ec6a288172be49a424ea4254f338db
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2025-08-31 16:04:18 -0600

    Update and rename ExamplePlugin.java to CAPlugin.java

---
+++ src/example/CAPlugin.java
@@ -0,0 +1,21 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.net.Administration.*;
+import mindustry.world.blocks.storage.*;
+
+public class CAPlugin extends Plugin{
+
+    public void init(){
+
+        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
+
+    }
+
+}
